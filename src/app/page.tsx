1ac4250b9e8a18bb5f48bb40063bca63b9c4ad1b
Brad White <bradscottwhite@gmail.com>
2024-04-16 11:27:58 -0400

    put queries in separate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,35 @@
+import { SignedIn, SignedOut } from '@clerk/nextjs';
 import { getMyImages } from '~/server/queries';
 
 export const dynamic = 'force-dynamic';
 
-export default async function HomePage() {
+export async function Images() {
 
-  //var images: any[] = [];
-  //try {
   const images = await getMyImages();
-  //} catch (error) {}
 
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className='w-48'>
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
-    </main>
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className='w-48'>
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
   );
+}
+
+export default async function HomePage() {
+  return (
+    <main className="">
+      <SignedOut>
+        <div className='h-full w-full text-center text-2xl'>
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
+    </main>
+  )
 }
